e66361b225e2d3375b1751031af0d1ae5384c08d
Souradeep30 <95559269+Souradeep30@users.noreply.github.com>
2022-03-08 21:06:21 +0530

    Add files via upload

---
+++ fsds python basic assgnmnt 3.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Answer 1:
+
+Increase modularity
+Increase code reusibility
+Reduce amount of code
+
+
+# In[ ]:
+
+
+Answer 2:
+
+The function run(or execute) when it is called. When function is called the control flow which go to first line of code present inside the function.
+
+
+# In[ ]:
+
+
+Answer 3:
+
+def function name_():
+
+
+# In[2]:
+
+
+#Answer 4:
+    
+"""Function: Function is set of codes which produce some result. 
+   Function Call: Function call is used to invoke or execute set of codes present inside the function."""
+
+def function_1():
+    return "call the function"
+    
+
+
+# In[3]:
+
+
+function_1()
+
+
+# In[ ]:
+
+
+Answer 5:
+    
+There are 1 global scopes and 1 local scope in the python program.
+
+
+# In[ ]:
+
+
+Answer 6:
+    
+The local scope gets destroyed and all the variables in it are forgotten.
+
+
+# In[11]:
+
+
+#Answer 7:
+    
+"""Return value is value, varible or expression which a function can return.
+   Yes function return value can be an expression"""
+
+def function_2 (a,b):
+    
+    return a+b
+
+function_2(50,50)
+
+
+# In[ ]:
+
+
+Answer 8:
+    
+It will return Nontype.
+
+
+# In[ ]:
+
+
+Answer 9:
+    
+This can be achieved by using global keyword to redefine the variable and make the changes of variable as global change.
+
+
+# In[ ]:
+
+
+Answer 10:
+    
+None data type is a null variable or null object.
+
+
+# In[ ]:
+
+
+Answer 11:
+    
+The statement "import areallyourpetsnamederic" will import the module or package areallyourpetsnamederic.
+
+
+# In[ ]:
+
+
+Answer 12:
+    
+After import the spam.
+spam.bacon()
+
+
+# In[ ]:
+
+
+Answer 13:
+    
+Together try and except block is used to save programme from crashing when an error is encountered.
+Try: Try block is used for any suspicious code which can throw a runtime error.
+Except: Except block is used to catch error if any encountered inside try block.
+ 
+
